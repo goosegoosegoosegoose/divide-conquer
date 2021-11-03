dc2ae0435824922a9209c8ede328f24d44647096
postgres <kazkobayashi96@gmail.com>
2021-11-03 14:15:26 -0700

    find-floor and readme

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,21 @@
-function findFloor() {
-  
+function findFloor(arr, num) {
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+
+  while (leftIdx <= rightIdx) {
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+    let rightVal = arr[middleIdx + 1];
+
+    if (middleVal < num && (rightVal > num || middleIdx == arr.length-1)) {
+      return middleVal
+    } else if (middleVal > num) {
+      rightIdx = middleIdx - 1
+    } else {
+      leftIdx = middleIdx + 1
+    }
+  }
+  return -1;
 }
 
 module.exports = findFloor
---
+++ readme.md
@@ -0,0 +1,4 @@
+# What I did
+- wrote functions that hopefully have time complexities of O(log(n))
+- still unsure how to test
+  - npm test only runs 3 out of 5 tests, unsure what's wrong there
