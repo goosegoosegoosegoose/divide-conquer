83fe7827e0f5704e6e5507859189d9fe919a75ab
postgres <kazkobayashi96@gmail.com>
2021-11-03 04:33:24 -0700

    find-rotated-index

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,36 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, num) {
+ let leftIdx = 0;
+ let rightIdx = arr.length - 1;
+ let median = Math.floor(arr.length / 2)
+
+ while (leftIdx <= rightIdx) {
+  let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+  let middleVal = arr[middleIdx];
+  let rightVal = arr[middleIdx + 1];
+
+    if (middleVal === num) {
+      return middleIdx;
+    } else if (rightVal === num) {
+      return middleIdx + 1;
+    } else if (middleVal > num && middleVal > rightVal && median < num) {
+      console.log(`middleVal is ${middleVal}`);
+      console.log(`middleIdx is ${middleIdx}`);
+      rightIdx = middleIdx - 1;
+    } else if (middleVal > num && middleVal > rightVal && median > num) {
+      console.log(`middleVal is ${middleVal}`);
+      console.log(`middleIdx is ${middleIdx}`);
+      leftIdx = middleIdx + 1;
+    }  else if (middleVal < num && middleVal < rightVal) {
+      console.log(`middleVal is ${middleVal}`);
+      console.log(`middleIdx is ${middleIdx}`);
+      leftIdx = middleIdx + 1;
+    } else if (middleVal > num && middleVal < rightVal) {
+      console.log(`middleVal is ${middleVal}`);
+      console.log(`middleIdx is ${middleIdx}`);
+      rightIdx = middleIdx - 1;
+    }
+  }
+  return -1;
 }
 
 module.exports = findRotatedIndex
